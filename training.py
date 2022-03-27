f30519ac1e9b9012cb31b046ff1937c650e8e212
antonram <antonram@usc.edu>
2022-03-26 18:28:31 -0700

    sad face path

--- training.py
+++ training.py
@@ -117,5 +117,5 @@ def emotionVideo(cap):
 if __name__ == '__main__':
     # camera = cv2.VideoCapture(0) # If you are using an USB Camera then Change use 1 instead of 0.
     # emotionVideo(camera)
-    IMAGE_PATH = "happy_frog.jpg"
+    IMAGE_PATH = "sad_face.jpg"
     emotionImage(IMAGE_PATH) # If you are using this on an image please provide the path
