2ffa77dd79e67563889ece05f23efc5910f523bc
antonram <antonram@usc.edu>
2022-03-27 09:52:31 -0700

    atoi

--- training.py
+++ training.py
@@ -48,16 +48,17 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     pass
 
-
+no = 0
 def rpi1_sound_callback(client, userdata, msg):
-    print("YES")
     print(str(msg.payload))
     if len(rpi1_values) < 3:
     # ADD EACH INDIVIDUAL DATA POINT
-        rpi1_values.append(int(msg.payload))
+        rpi1_values.append(atoi(msg.payload))
         if len(rpi1_values) == 3:
             if(rpi1_values[0] > 300 and rpi1_values[1] > 300 and rpi1_values[2] > 300):
-                no = 1
+                no += 1
+                if no == 100:
+                    no = 0
             rpi1_values = []
                 
    
